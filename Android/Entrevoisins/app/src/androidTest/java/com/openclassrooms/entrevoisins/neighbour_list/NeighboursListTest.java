5d8a3b7bd9eb90246299268bda195274e19c3b92
Praal0 <guerrero.dorian1@gmail.com>
2021-02-01 17:10:05 +0100

    P3_Dorian_Guerrero_Update_Test
    Update : Test with notification
    (A completer)

--- Android/Entrevoisins/app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
+++ Android/Entrevoisins/app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
@@ -1,11 +1,17 @@
 package com.openclassrooms.entrevoisins.neighbour_list;
 
 import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject2;
+import android.support.test.uiautomator.Until;
+import android.support.test.uiautomator.By;
+
 
 import com.openclassrooms.entrevoisins.R;
 import com.openclassrooms.entrevoisins.model.Neighbour;
@@ -18,17 +24,17 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.List;
+import static org.junit.Assert.assertEquals;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.openclassrooms.entrevoisins.utils.RecyclerViewItemCountAssertion.withItemCount;
-import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.core.IsNull.notNullValue;
-
+import static org.junit.Assert.assertTrue;
 
 
 /**
@@ -43,9 +49,28 @@ public class NeighboursListTest {
     private Neighbour fakeInfoNeighbour;
     private String facebookNeighbourg;
     private String aboutMeNeighbour;
+    private static final String BASIC_SAMPLE_PACKAGE
+            = "com.example.android.testing.uiautomator.BasicSample";
+    private static final int LAUNCH_TIMEOUT = 5000;
+    private static final String STRING_TO_BE_TYPED = "UiAutomator";
+    private UiDevice device;
 
 
     private ListNeighbourActivity mActivity;
+
+    @Before
+    public void startMainActivityFromHomeScreen() {
+
+        // Start from the home screen
+        device.pressHome();
+
+        // Wait for launcher
+        final String launcherPackage = device.getLauncherPackageName();
+        assertThat(launcherPackage, notNullValue());
+        device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)),
+                LAUNCH_TIMEOUT);
+
+    }
 
     @Rule
     public ActivityTestRule<ListNeighbourActivity> mActivityRule =
@@ -55,12 +80,16 @@ public class NeighboursListTest {
 
     @Before
     public void setUp() {
+        UiDevice mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
         mActivity = mActivityRule.getActivity();
         assertThat(mActivity, notNullValue());
         fakeInfoNeighbour = new Neighbour(1, "Caroline", "https://i.pravatar.cc/150?u=a042581f4e29026704d", "Saint-Pierre-du-Mont ; 5km",
                 "+33 6 86 57 90 14",  "Bonjour !Je souhaiterais faire de la marche nordique. Pas initiée, je recherche une ou plusieurs personnes susceptibles de m'accompagner !J'aime les jeux de cartes tels la belote et le tarot..",false);
         facebookNeighbourg = "www.Facebook.com/" + fakeInfoNeighbour.getName();
         aboutMeNeighbour = fakeInfoNeighbour.getAboutMe();
+        // Initialize UiDevice instance
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
     }
 
     /**
@@ -96,7 +125,7 @@ public class NeighboursListTest {
     }
 
     @Test
-    public void myNeighbourDetail_TestFabSnack() {
+    public void myNeighbourDetail_TestFabSnackBar() {
         // Given : We launch InfoNeighbourActivity with fake neighbour.
         Intent intent = new Intent();
         intent.putExtra("neighbour_Id",fakeInfoNeighbour.getId());
@@ -118,16 +147,57 @@ public class NeighboursListTest {
     @Test
     public void myNeighbourDetail_CheckInfo(){
         // Given : We Click on Caroline to open Detail of neighbour
-        onView(ViewMatchers.withId(R.id.list_neighbours))
+        onView(withId(R.id.list_neighbours))
         .perform(RecyclerViewActions.actionOnItemAtPosition(0, ViewActions.click()));
         // Then : We Check if data is correct
-        //onView(ViewMatchers.withId(R.id.neighbour_Avatar)).check(matches(ViewMatchers.wit);
-        onView(ViewMatchers.withId(R.id.textViewName)).check(matches(ViewMatchers.withText(fakeInfoNeighbour.getName())));
-        onView(ViewMatchers.withId(R.id.textViewNameNeighbourg)).check(matches(ViewMatchers.withText(fakeInfoNeighbour.getName())));
-        onView(ViewMatchers.withId(R.id.textAdresseNeighbour)).check(matches(ViewMatchers.withText(fakeInfoNeighbour.getAddress())));
-        onView(ViewMatchers.withId(R.id.textViewphoneNeighbour)).check(matches(ViewMatchers.withText(fakeInfoNeighbour.getPhoneNumber())));
-        onView(ViewMatchers.withId(R.id.textViewFaceBook)).check(matches(ViewMatchers.withText(facebookNeighbourg)));
-        //onView(ViewMatchers.withId(R.id.textViewAboutMe)).check(matches(ViewMatchers.withText(fakeInfoNeighbour.getAboutMe())));
+        onView(withId(R.id.textViewName)).check(matches(withText(fakeInfoNeighbour.getName())));
+        onView(withId(R.id.textViewNameNeighbourg)).check(matches(withText(fakeInfoNeighbour.getName())));
+        onView(withId(R.id.textAdresseNeighbour)).check(matches(withText(fakeInfoNeighbour.getAddress())));
+        onView(withId(R.id.textViewphoneNeighbour)).check(matches(withText(fakeInfoNeighbour.getPhoneNumber())));
+        onView(withId(R.id.textViewFaceBook)).check(matches(withText(facebookNeighbourg)));
+        onView(withId(R.id.textAboutMe)).check(matches(withText(fakeInfoNeighbour.getAboutMe())));
+    }
+
+    @Test
+    public void addNeighbourFavories(){
+        // Given : We click on Caroline to open detail
+        onView(ViewMatchers.withId(R.id.list_neighbours))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, ViewActions.click()));
+        // When perform click on fab to add favorite neighbour
+        onView(withId(R.id.floatingButtonFavorie)).perform(ViewActions.click());
+        // When Return to list
+        onView(withId(R.id.buttonBack)).perform(ViewActions.click());
+        // When click to tab view favories
+        onView(ViewMatchers.withText("FAVORIES")).perform(ViewActions.click());
+        // Then : the number of element is 11
+        onView(ViewMatchers.withId(R.id.list_fav_neighbours)).check(withItemCount(ITEMS_EMPTY+1));
+
+    }
+
+    @Test
+    public void myNeighbourDetail_TestFabNotification(){
+        String expectedTitle = "Ajout Favorie";
+        String expectedText = "Caroline ";
+
+        // Given : We launch InfoNeighbourActivity with fake neighbour.
+        Intent intent = new Intent();
+        intent.putExtra("neighbour_Id",fakeInfoNeighbour.getId());
+        intent.putExtra("neighbour_Name",fakeInfoNeighbour.getName());
+        intent.putExtra("neighbour_AboutMe",fakeInfoNeighbour.getAboutMe());
+        intent.putExtra("neighbour_AvatarUrl",fakeInfoNeighbour.getAvatarUrl());
+        intent.putExtra("neighbour_PhoneNumber",fakeInfoNeighbour.getPhoneNumber());
+        intent.putExtra("neighbour_Adresse",fakeInfoNeighbour.getAddress());
+        intent.putExtra("neighbour_isFavorite",fakeInfoNeighbour.isFavorite());
+        InfoNeighbourActivityTestRule.launchActivity(intent);
+        // When perform click on fab to add favorite neighbour
+        onView(withId(R.id.floatingButtonFavorie)).perform(ViewActions.click());
+        // We open notification
+        device.openNotification();
+        UiObject2 title = device.findObject(By.text(expectedText));
+        UiObject2 text = device.findObject(By.textStartsWith(expectedText));
+        assertEquals(expectedTitle, title.toString());
+        assertTrue(text.equals(expectedText));
+        device.clearLastTraversedText();
     }
 
 }
